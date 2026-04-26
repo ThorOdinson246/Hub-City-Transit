06d5dc146faa0be9d41b1e9802d5851f29c0d480
ThorOdinson246 <mukeshpoudel246@gmail.com>
2026-04-25 20:08:16 -0500

    chore: project structure and dependency cleanup

--- test_camera.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:flutter/widgets.dart';
-
-void main() {
-  final fit = CameraFit.bounds(bounds: LatLngBounds(const LatLng(0,0), const LatLng(1,1)));
-  print(fit.toString());
-}
